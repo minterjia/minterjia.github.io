55103fef7a2c43b6aa09d00c9e3c4cf0baa21f6b
minterjia <61584074+minterjia@users.noreply.github.com>
2022-07-24 10:23:20 +0800

    Add files via upload

---
+++ js/jscustom.js
@@ -0,0 +1,30 @@
+function refreshCache() {
+	"serviceWorker" in window.navigator && navigator.serviceWorker.controller ? confirm("是否确定刷新博文缓存") && navigator.serviceWorker
+		.controller.postMessage("refresh") : GLOBAL_CONFIG.Snackbar ? btf.snackbarShow("ServiceWorker未激活") : alert(
+			"ServiceWorker未激活")
+}
+
+function fuckyou() {
+	window.close(), window.location = "about:blank"
+}
+
+function click(e) {
+	document.all && (2 != event.button && 3 != event.button || (alert("欢迎光临，有什么需要帮忙的话，请与站长联系！谢谢您的合作！！！"), oncontextmenu =
+		"return false")), document.layers && 3 == e.which && (oncontextmenu = "return false")
+}
+navigator.serviceWorker.addEventListener("message", (e => {
+		"success" === e.data && location.reload(!0)
+	})), "http://localhost:4000/" != window.location.href && "https://sianx.com/" != window.location.href &&
+	"https://www.sianx.com/" != window.location.href && "https://blog.sianx.com/" != window.location.href || "" == window.name &&
+	(window.name = "hytc", Snackbar.show({
+		text: "欢迎来到丨浅笑安然丨的小站！",
+		actionText: "关闭",
+		actionTextColor: "#ffffff",
+		pos: "bottom-center",
+		duration: "2000"
+	})), document.layers && (fuckyou(), document.captureEvents(Event.MOUSEDOWN)), document.onmousedown = click, document.oncontextmenu =
+	new Function("return false;"), document.onkeydown = document.onkeyup = document.onkeypress = function() {
+		if (123 == window.event.keyCode) return fuckyou(), window.event.returnValue = !1, !1
+	}, $(document).keydown((function(e) {
+		if (1 == e.ctrlKey && 83 == e.keyCode) return console.log("ctrl+s"), !1
+	}));
